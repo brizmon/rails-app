870ed7863d621b5b0761561acba465f45ecd45d9
Briseida Montiel <brizism@gmail.com>
2017-09-06 11:46:57 -0400

    rails file structure set up

---
+++ project_app/db/seeds.rb
@@ -0,0 +1,39 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ title: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Todo.create(
+[
+  {
+    title: "Call Mom",
+    description: "Today Joockee touch bright hot energy.  Can not explain so we invent Thor.  Thor invent 'fire'."
+  },
+  {
+    title: "Make sushi rolls",
+    description: "Today I observed it takes 4 rocks and 2 sticks to sink duck.  How do Joockee and Ja-Ja make large duck to ride?"
+  },
+  {
+    title: "Ja-Ja",
+    description: "9 ducks will not float 1 Ja-Ja.  On positive side, Ja-Ja invented towel that dry Ja-Ja nicely."
+  },
+  {
+    title: "Joockee",
+    description: "Today I chase ducks with Thor flame. Ducks hate Thor flame.  Slow ducks become fast ducks."
+  },
+  {
+    title: "Joockee",
+    description: "45-duck-boat carries 1 Joockee and a Ja-Ja.  0-60 in 4 hours!  Success."
+  },
+  {
+    title: "Ja-Ja",
+    description: "We chase 45-duck-boat with 3-log-Thor-flame.  Duck boat much fast now."
+  },
+  {
+    title: "Ja-Ja",
+    description: "Duck boat complete.  Next mission - invent purple."
+  }
+]
+)
