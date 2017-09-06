9c60bb1085e6b41ad55a234ed04c1d13b2613b12
Briseida Montiel <brizism@gmail.com>
2017-09-06 18:46:09 -0400

    add index show create destroy and update methods to TodoController

--- project_app/app/controllers/todo_controller.rb
+++ project_app/app/controllers/todo_controller.rb
@@ -1,2 +1,29 @@
 class TodoController < ApplicationController
+    def index 
+        @todos = Todo.all
+    end
+
+    def show
+        @todo = Todo.find_by(id: params[:id])
+    end 
+
+    def create 
+        todo = params['todo']
+        Todo.create(title: todo['title'],
+                    description: todo['description'])
+        redirect_to todos_path
+    end
+
+    def destroy 
+        Todo.destroy(params['id'])
+        redirect_to todos_path
+    end
+
+    def update 
+        todo = params['todo']
+        Todo.update(params[:id],
+                    title: todo['title'],
+                    description: todo['description'])
+        redirect_to todos_path
+    end
 end
